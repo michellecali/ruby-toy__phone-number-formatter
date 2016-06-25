e510ad8110859a21a8758e10116d03517d35e9dc
Michelle Cali <mctexx@gmail.com>
2016-06-25 13:48:19 -0500

    submitting untested code

--- lib/phone_number_formatter.rb
+++ lib/phone_number_formatter.rb
@@ -2,5 +2,14 @@
 # returns a properly formatted phone number.
 
 def format_phone_number(phone_number_str)
+	array = []
+	array = phone_number_str.split("")
+	phone_number_str = ""
+	array.keep_if { |i| i =~ /[0123456789]/ } 
+	array.insert(0,"(") 
+	array.insert(4,")")
+	array.insert(8,"-")
+	array.insert(5," ")
+	phone_number_str = array.join
   return phone_number_str
 end
